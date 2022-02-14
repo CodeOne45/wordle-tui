71537b838c86bc57db5c0e7926fe31af3fa72315
Aman KUMAR <57890543+CodeOne45@users.noreply.github.com>
2022-02-14 09:55:40 +0100

    feat: Added wordle game main struture

---
+++ Rakefile
@@ -0,0 +1,5 @@
+task default: %w[test]
+
+task :run do
+  ruby 'lib/main.rb'
+end
---
+++ lib/wordle/game.rb
@@ -0,0 +1,79 @@
+module Wordle
+    class Game
+        def self.play
+            new.play
+        end
+
+        # Global variables
+        def initialize
+            @words_list = List.new
+            @options = read_option
+            # Get a word to guess
+            @word_to_guess = generate_word
+        end
+
+        def play 
+
+            over = false
+
+            Console.print
+
+            puts "Guess a 5 letter word:"
+            guesses = []
+
+            while guesses.length <= 5 && !over
+                # Get player input
+                input_guess = gets.chomp
+
+                guess_valid = GuessValidator.new(guess, @words_list)
+                if guess_valid.invalid?
+                    puts guess_valid.errors
+                    next
+                end 
+
+                analyzer = GuessAnalyzer.new(@word_to_guess, input_guess)
+                # print input word with colors
+                puts analyzer.colors
+                # Add guess world into the guesses list
+                guesses << analyzer.squares
+
+                if analyzer.matche?
+                    over = true
+                    break
+                end
+            end
+
+            puts guesses
+        end
+
+        private
+
+        def generate_word
+            if @options[:identifier]
+                @list.by_hash(@options[:identifier])
+            else
+                @list.random
+            end
+        end
+
+        def read_options
+            options = {}
+            parser = OptionParser.new do |opts|
+              opts.banner = "Usage: wordle [options]"
+      
+              opts.on("-iIDENTIFIER", "--identifier=IDENTIFIER", "Pass word identifer to target a specific word that someone else has played. Identifier gets printed at the end of the game to share.") do |i|
+                options[:identifier] = i
+              end
+            end
+      
+            begin
+              parser.parse!
+            rescue OptionParser::InvalidOption
+              puts "Option not recognized"
+            end
+      
+            options
+          end
+
+
+
